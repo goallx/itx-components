348f6fa111d00e9a317cbd8f605e418e56741d7a
wael.haddad <team@goallx.com>
2025-09-04 15:35:48 +0300

    done

--- my-app/src/app/courses/page.tsx
+++ my-app/src/app/courses/page.tsx
@@ -4,54 +4,14 @@ import React, { useEffect, useState } from "react";
 import CoursesSection, { ICourse } from "./components/Courses";
 import { createClient } from "@/utils/supabase/client";
 import { useSearchParams } from "next/navigation";
+import CourseCardSkeleton from "./loading";
 
 export default function CoursesPage() {
-    // const [user, setUser] = useState<any>(null);
-
-    // const [loading, setLoading] = useState(true);
-
-    // useEffect(() => {
-    //     const handleMessage = (event: MessageEvent) => {
-    //         // Only accept messages from Framer
-    //         const allowedOrigins = ["https://itx-academy.com"];
-    //         if (!allowedOrigins.includes(event.origin)) return;
-
-    //         const { type, user: incomingUser } = event.data || {};
-    //         if (type === "AUTH_STATE_CHANGE" && incomingUser) {
-    //             setUser(incomingUser);
-    //             fetchCourses(incomingUser.id);
-    //         }
-    //     };
-
-    //     window.addEventListener("message", handleMessage);
-
-    //     // Notify parent that iframe is ready
-    //     window.parent.postMessage({ type: "READY_FOR_USER" }, "https://itx-academy.com");
-
-    //     return () => window.removeEventListener("message", handleMessage);
-    // }, []);
-
-
-    // useEffect(() => {
-    //     const fetchCourses = async () => {
-    //         const userRes = await fetch('https://itx-components.vercel.app/api/auth/user', {
-    //             credentials: 'include',
-    //         })
-    //         const data = await userRes.json()
-    //         console.log('@@user data', data)
-    //         setUser(data.user)
-    //     }
-    //     fetchCourses()
-    // }, [])
-
-    // if (loading) return <p className="text-center py-20">Loading courses...</p>;
-    // if (!user) return <p className="text-center py-20">Waiting for login...</p>;
 
     const searchParams = useSearchParams();
     const userId = searchParams.get("user");
     const [loading, setLoading] = useState<boolean>(false)
     const [courses, setCourses] = useState<(ICourse & { isSubscribed: boolean })[]>([]);
-    console.log('@@userid', userId)
 
     useEffect(() => {
         if (userId) {
@@ -70,13 +30,11 @@ export default function CoursesPage() {
             return;
         }
 
-        // Fetch user subscriptions
         const { data: subscriptions } = await supabase
             .from("user_courses")
             .select("course_id")
             .eq("user_id", userId);
 
-        console.log('@@coures', subscriptions)
         const subscribedCourseIds = subscriptions?.map(s => s.course_id) || [];
 
         const mapped = coursesData.map(course => ({
@@ -88,7 +46,7 @@ export default function CoursesPage() {
         setLoading(false);
     };
 
-    if (loading) return <p className="text-center py-20">Loading courses...</p>;
+    if (loading) return <CourseCardSkeleton />
 
     return <CoursesSection courses={courses} />;
 
